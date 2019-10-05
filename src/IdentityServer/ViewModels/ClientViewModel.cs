b91f10c3786cebb249d9155ddc9cb269dbafeac6
Diony S. Castro <dnycstr@gmail.com>
2019-10-05 22:16:26 +0800

    Add New Client

---
+++ src/IdentityServer/ViewModels/ClientViewModel.cs
@@ -0,0 +1,22 @@
+﻿
+using IdentityServer4.EntityFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.ViewModels
+{
+    public class ClientViewModel : Client
+    {
+        
+    }
+
+    public class AddClientViewModel : ClientViewModel
+    {
+    }
+
+    public class EditClientViewModel : ClientViewModel
+    {
+    }
+}
---
+++ src/IdentityServer/wwwroot/viewscripts/manageclients/clientsdatatable.js
@@ -0,0 +1,35 @@
+﻿
+var datatableurl = $("#datatableurl").val();
+
+var table = $('#clients-dataTable').DataTable({
+    "processing": true,
+    "serverSide": true,
+    "ajax":
+    {
+        "url": datatableurl,
+        "type": "POST",
+        "dataType": "JSON"
+    },
+    rowId: "Id",
+    "columns": [
+        { "data": "ClientId" },
+        { "data": "ClientName" },
+        { "data": "Enabled" }
+    ]
+});
+
+table
+    .buttons()
+    .container()
+    .appendTo('#table-control-panel');
+
+
+$('#clients-dataTable tbody').on('click', 'tr', function () {
+    if ($(this).hasClass('selected')) {
+        $(this).removeClass('selected');
+    }
+    else {
+        table.$('tr.selected').removeClass('selected');
+        $(this).addClass('selected');
+    }
+});
